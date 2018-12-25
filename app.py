496f1cf03fbb889538fc33de3f71e08bfee02b27
Supuni Nimeshika <Supuni.Nimeshika@gmail.com>
2018-12-25 15:08:34 +0530

    Add Flask Server Configuration

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+import requests
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+    print ('Sadeepa')
     return 'Hello World!'
 
 
+@app.route('/accessToken/<token>' , methods=['POST'])
+def fb_data(token):
+    print ('access')
+    print(str(token))
+    re = 'https://graph.facebook.com/v3.2/me/posts?fields=comments%2Cmessage%2Cfull_picture&access_token='+token
+    me1 = requests.get(re)
+    # f1 =requests.get(friends)
+
+    print(me1.text);
+    return me1.text
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host= '0.0.0.0' , port=9091, debug=False)
