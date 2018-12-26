9644196046bf0fcaf276c38136a7b45374e775ec
Supuni Nimeshika <Supuni.Nimeshika@gmail.com>
2018-12-26 20:44:50 +0530

    Get Access Token Automatically When Signing

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import json
 import requests
 
 
@@ -7,8 +8,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    token='EAAbJHUYwk6YBAPDiXQkGWvGyr9wBNVu4T5JCHUhDqCg8DMxGBw1pixjLVTC9EW9EEoUbzS6DzZBvihXJZAIegVaBpT1OnLtZCZBc1Gfc2Fii83QdLcybIMCinQ2AZBn8i7cyp7NrrVhLqkZBPVuhEAVWDikDQ4jwUYtMcRnxfNbUzYame394RqqwZBlrBx0rkzys7zeB1FO1vfW5FG0etFDiBojYyoVs4cZD'
     print ('Sadeepa')
-    return 'Hello World!'
+    re = 'https://graph.facebook.com/v3.2/me/posts?fields=comments%2Cmessage%2Cfull_picture&access_token=' + token
+    me1 = requests.get(re)
+    # f1 =requests.get(friends)
+
+    # print(me1.json());
+    data =me1.json();
+    json_array =json.dumps(data);
+    a= json.loads(json_array)
+    store_list = []
+    print(a['data'])
+
+    for item in a['data']:
+
+
+        try:
+            image_url = item['full_picture']
+            print(image_url)
+        except KeyError:
+            print ("Haven't full picture")
+
+
+
+    return jsonify(me1.json())
+
 
 
 @app.route('/accessToken/<token>' , methods=['POST'])
@@ -19,8 +44,22 @@ def fb_data(token):
     me1 = requests.get(re)
     # f1 =requests.get(friends)
 
-    print(me1.text);
-    return me1.text
+    data = me1.json();
+    json_array = json.dumps(data);
+    a = json.loads(json_array)
+    store_list = []
+    print(a['data'])
+
+    for item in a['data']:
+
+        try:
+            image_url = item['full_picture']
+            store_list.append(image_url)
+        except KeyError:
+            print("Haven't full picture")
+
+
+    return jsonify(store_list)
 
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0' , port=9091, debug=False)
+    app.run(host= '0.0.0.0' , port=9091, debug=True)
